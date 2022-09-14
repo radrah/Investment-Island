0fd4621b5cd2d925793d0f586bc18be77d5f28f6
Lucy <lzlucyz@gmail.com>
2022-09-14 23:35:28 +1200

    Fixed added profile component

--- .history/pages/index_20220914233401.tsx
+++ .history/pages/index_20220914233401.tsx
@@ -29,13 +29,6 @@ const Home: NextPage = () => {
     <>
       <IslandImage />
 
-      <h2>Profile</h2>
-      <Profile
-        header="Profile"
-        description="This is your profile"
-        content={content}
-      />
-
       <h1>Sign up placeholder here:</h1>
       <div>
         <SignUpBox />
