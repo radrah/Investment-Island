0982de286c3ed6a9a783cf84951ff86b7717b47a
boxy8 <zou.emily@yahoo.co.nz>
2022-09-19 14:17:56 +1200

    Add basic design of sign up popup

---
+++ components/SignUpBox.tsx
@@ -0,0 +1,89 @@
+import Button from '@mui/material/Button';
+import Modal from '@mui/material/Modal';
+import Typography from '@mui/material/Typography';
+import TextField from '@mui/material';
+import createUser, {
+  createFacebookUser,
+  createUserGoogle,
+} from "../firebase/authCreateUser";
+import { useState } from "react";
+import styles from "./SignUpBox.module.css";
+
+export default function SignUpBox() {
+  // pop up handler
+  const [open, setOpen] = useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+
+  // sign in handler
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSignIn = () => {
+    createUser(email, password);
+    setEmail("");
+    setPassword("");
+  };
+
+  return (
+    <div>
+      // replace with sign up button
+      <Button className={styles.signup_button} onClick={handleOpen}>Sign in</Button>
+
+      <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <div className={styles.popup}>
+          <div className={styles.credentials_box}>
+            <Typography>credentials</Typography>
+          </div>
+
+          <Button variant="contained" className={styles.button}>
+            Sign in with Google
+          </Button>
+          <Button variant="contained" className={styles.button}>
+            Sign in with Facebook
+          </Button>
+        </div>
+      </Modal>
+      {/* <input
+        type="email"
+        name="email"
+        placeholder="Email"
+        onChange={(e) => {
+          setEmail(e.target.value);
+        }}
+        value={email}
+      />
+      <br />
+      <input
+        type="password"
+        name="password"
+        placeholder="Password"
+        onChange={(e) => {
+          setPassword(e.target.value);
+        }}
+        value={password}
+      />
+      <br />
+      <button onClick={handleSignIn}>Sign Up</button>
+      <button
+        onClick={() => {
+          createUserGoogle();
+        }}
+      >
+        Sign Up With Google
+      </button>
+      <button
+        onClick={() => {
+          createFacebookUser();
+        }}
+      >
+        Sign Up With Facebook
+      </button> */}
+    </div>
+  );
+}
