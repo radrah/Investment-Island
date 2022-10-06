3e2d709f2ed322768f04c4873d021c68265f5cdb
boxy8 <zou.emily@yahoo.co.nz>
2022-10-06 21:23:56 +1300

    style link and input fields

--- components/SignUpBox.tsx
+++ components/SignUpBox.tsx
@@ -2,7 +2,7 @@ import Button from '@mui/material/Button';
 import Modal from '@mui/material/Modal';
 import Typography from '@mui/material/Typography';
 import TextField from '@mui/material/TextField';
-import Link from '@mui/material/Link'
+import Link from '@mui/material/Link';
 
 import createUser, {
   createFacebookUser,
@@ -39,20 +39,56 @@ export default function SignUpBox() {
         aria-describedby="modal-modal-description"
       >
         <div className={styles.popup}>
-            <div className={styles.credentials_box}>
-              <img className={styles.avatar} src="assets/static/user.png"></img>
-              <div className={styles.input_field}>
-                <img src="assets/static/mini_user.png" className={styles.credentials_icon}></img>
-                <TextField variant="standard" placeholder="Username" size="small"  
-                InputProps={{disableUnderline: true}}/>
+          <div className={styles.credentials_box}>
+            <img className={styles.avatar} src="assets/static/user.png"></img>
+            <div className={styles.input_box}>
+              <img src="assets/static/mini_user.png" className={styles.credentials_icon}></img>
+                <TextField variant="standard" placeholder="Username" className={styles.input_field} 
+                  sx={{
+                    input: {
+                      fontWeight: 600,
+                      fontSize: '1.2rem',
+                      "&::placeholder": {
+                        fontWeight: 600,
+                        fontSize: '1.2rem',
+                        opacity: 1,
+                        color: 'white'
+                      },
+                    }
+                  }}
+                  InputProps={{disableUnderline: true}}
+                />
               </div>
-              <div className={styles.input_field}>
-              <img src="assets/static/padlock.png" className={styles.credentials_icon}></img>
-                <TextField variant="standard" placeholder="Password" size="small"              
-                InputProps={{disableUnderline: true}}/>
-              </div>
-              <Typography>Don't have an account?</Typography>
-              <Link component="button" onClick={() => {}}>Register here!</Link>
+              <div className={styles.input_box}>
+                <img src="assets/static/padlock.png" className={styles.credentials_icon}></img>
+                <TextField variant="standard" type="password" placeholder="Password" className={styles.input_field} 
+                  sx={{
+                    input: {
+                      fontWeight: 600,
+                      fontSize: '1.2rem',
+                      "&::placeholder": {
+                        fontWeight: 600,
+                        fontSize: '1.2rem',
+                        opacity: 1,
+                        color: 'white'
+                      },
+                    }
+                  }}
+                  InputProps={{disableUnderline: true}}
+                />
+            </div>
+            <Typography sx={{fontSize: '1.2rem'}}>Don't have an account?</Typography>
+            <Link
+              component="button"
+              variant="body1"
+              onClick={() => { }}
+              sx={{
+                fontWeight: 600,
+                fontSize: '1.2rem'
+              }}
+            >
+              Register here!
+            </Link>
           </div>
 
           <Button variant="contained" className={styles.button}>
